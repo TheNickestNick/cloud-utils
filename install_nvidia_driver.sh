375a8e54bab17e83553116fb85fa2b69eb1aa3e1
nickaceves <nickaceves@gmail.com>
2022-08-20 14:59:30 -0700

    Update install_nvidia_driver.sh

--- install_nvidia_driver.sh
+++ install_nvidia_driver.sh
@@ -1,2 +1,5 @@
+sudo apt update
+sudo apt install build-essential
+
 wget https://us.download.nvidia.com/tesla/450.203.03/NVIDIA-Linux-x86_64-450.203.03.run
 sudo bash NVIDIA-Linux-x86_64-450.203.03.run
