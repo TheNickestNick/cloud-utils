97c8edb0ff1ff875a50e5c82027161f5e762b531
nickaceves <nickaceves@gmail.com>
2022-08-20 16:07:51 -0700

    Reorganize files that don't work

--- cuda/install_driver_and_10_2.sh
+++ cuda/install_driver_and_10_2.sh
@@ -1,2 +1,4 @@
+sudo apt update
+sudo apt -y install build-essential
 wget https://us.download.nvidia.com/tesla/440.118.02/NVIDIA-Linux-x86_64-440.118.02.run
 sudo sh ./NVIDIA-Linux-x86_64-440.118.02.run
