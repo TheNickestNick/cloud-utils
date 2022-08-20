92adc05abe37feeaca6168f79fd8ba9ba908f71f
nickaceves <nickaceves@gmail.com>
2022-08-20 15:20:29 -0700

    Add a buncha shit

--- update_nvidia_gpg_key.sh
+++ update_nvidia_gpg_key.sh
@@ -1,7 +1,5 @@
-# Currently supports only Ubuntu 20.04 x64
-distro=ubuntu2004
-arch=x86_64
-
 sudo apt-key del 7fa2af80
-wget https://developer.download.nvidia.com/compute/cuda/repos/$distro/$arch/cuda-keyring_1.0-1_all.deb
+distribution=$(. /etc/os-release;echo $ID$VERSION_ID | sed -e 's/\.//g')
+wget https://developer.download.nvidia.com/compute/cuda/repos/$distribution/x86_64/cuda-keyring_1.0-1_all.deb
 sudo dpkg -i cuda-keyring_1.0-1_all.deb
+sudo apt-get update
