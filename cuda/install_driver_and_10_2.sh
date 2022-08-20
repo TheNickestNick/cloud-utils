acdf306fb6f3dc41679db52794797e62ef646856
nickaceves <nickaceves@gmail.com>
2022-08-20 16:03:33 -0700

    Update install_driver_and_10_2.sh

--- cuda/install_driver_and_10_2.sh
+++ cuda/install_driver_and_10_2.sh
@@ -1,2 +1,2 @@
 wget https://us.download.nvidia.com/tesla/440.118.02/NVIDIA-Linux-x86_64-440.118.02.run
-sh https://us.download.nvidia.com/tesla/440.118.02/NVIDIA-Linux-x86_64-440.118.02.run
+sh ./NVIDIA-Linux-x86_64-440.118.02.run
