40929bf1364f122d0c9e5513d9557abad40312fe
nickaceves <nickaceves@gmail.com>
2022-08-20 15:01:09 -0700

    Skip confirmation for build-essential

--- install_nvidia_driver.sh
+++ install_nvidia_driver.sh
@@ -1,5 +1,5 @@
 sudo apt update
-sudo apt install build-essential
+sudo apt -y install build-essential
 
 wget https://us.download.nvidia.com/tesla/450.203.03/NVIDIA-Linux-x86_64-450.203.03.run
 sudo bash NVIDIA-Linux-x86_64-450.203.03.run
