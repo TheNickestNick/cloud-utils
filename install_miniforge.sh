3abd15bc6f45092b401d78c2a8687ef3a6bca4c6
nickaceves <nickaceves@gmail.com>
2022-08-20 16:11:30 -0700

    Update install_miniforge.sh

--- install_miniforge.sh
+++ install_miniforge.sh
@@ -1,3 +1,4 @@
 wget https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge3-Linux-x86_64.sh
 bash ./Miniforge3-Linux-x86_64.sh -b
-conda init bash
+
+~/miniforge3/bin/conda init bash
